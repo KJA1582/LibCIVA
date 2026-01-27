9893cf8aa5c2d406f72604a6d473a5864e6bb3ed
Kilian Hofmann <llego03@yahoo.com>
2026-01-27 20:37:15 +0100

    Logger added

---
+++ libciva/logger/logger.cpp
@@ -0,0 +1,15 @@
+#include "logger.h"
+
+Logger *Logger::singleton_ = nullptr;;
+
+Logger::Logger() {
+  file = std::ofstream("log.txt", std::ios::boolalpha);
+}
+
+Logger &Logger::GetInstance() noexcept {
+  if (singleton_ == nullptr) {
+    singleton_ = new Logger();
+  }
+
+  return *singleton_;
+}
---
+++ libciva/logger/logger.h
@@ -0,0 +1,24 @@
+#include <fstream>
+
+class Logger {
+    std::ofstream file;
+
+protected:
+  Logger();
+
+  static Logger *singleton_;
+
+public:
+  Logger(Logger &other) = delete;
+  void operator=(const Logger &) = delete;
+
+  template<typename T>
+  Logger &operator<<(const T &val) {
+    this->file << val;
+    file.flush();
+
+    return *this;
+  }
+
+  static Logger &GetInstance() noexcept;
+};
